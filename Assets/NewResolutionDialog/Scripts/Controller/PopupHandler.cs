7b3fa2868848034c7f2d698df954c88decb26a05
Steffen Itterheim <s.itterheim@stollvongati.com>
2020-05-15 16:12:04 +0200

    ensure dropdowns always visible, ensure canvas visible on launch, disable canvas by default

--- Assets/NewResolutionDialog/Scripts/Controller/PopupHandler.cs
+++ Assets/NewResolutionDialog/Scripts/Controller/PopupHandler.cs
@@ -11,11 +11,9 @@ public class PopupHandler : MonoBehaviour
 
     void Start()
     {
-        if (settings.dialogStyle == ResolutionDialogStyle.PopupDialog)
-        {
-            dialogCanvas.enabled = false;
+        dialogCanvas.enabled = (settings.dialogStyle == ResolutionDialogStyle.LaunchDialog);
+        if (dialogCanvas.enabled == false)
             StartCoroutine(WaitForActivation());
-        }
     }
 
     IEnumerator WaitForActivation()
@@ -26,6 +24,8 @@ public class PopupHandler : MonoBehaviour
 
             // toggle canvas
             dialogCanvas.enabled = !dialogCanvas.enabled;
+
+            // wait twice (into next frame) to prevent the hotkey from being recognized again in the same frame
             yield return new WaitForEndOfFrame();
             yield return new WaitForEndOfFrame();
         }
